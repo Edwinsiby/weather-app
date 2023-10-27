ab5884af42bd05a7fc0354b50ec68d28508004cc
edwinsiby <edwinsibyrajakumary@gmail.com>
2023-10-27 11:39:14 +0530

    initial

---
+++ main.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"gopkg.in/resty.v1"
+)
+
+type WeatherData struct {
+	Main struct {
+		Temp float64 `json:"temp"`
+	} `json:"main"`
+	Weather []struct {
+		Description string `json:"description"`
+	} `json:"weather"`
+}
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	apiKey := os.Getenv("OPENWEATHERMAP_API_KEY")
+
+	fmt.Println(apiKey)
+
+	router := gin.Default()
+
+	router.LoadHTMLGlob("templates/*")
+
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(200, "index.html", nil)
+	})
+
+	router.POST("/weather", func(c *gin.Context) {
+		city := c.PostForm("city")
+
+		client := resty.New()
+		resp, err := client.R().
+			SetQueryParams(map[string]string{
+				"q":     city,
+				"appid": apiKey,
+			}).
+			Get("https://api.openweathermap.org/data/2.5/weather")
+
+		if err != nil {
+			log.Println(err)
+			c.JSON(500, gin.H{
+				"error": "An error occurred while fetching weather data",
+			})
+			return
+		}
+
+		if resp.StatusCode() != 200 {
+			c.JSON(resp.StatusCode(), gin.H{
+				"error": "Failed to fetch weather data",
+			})
+			return
+		}
+
+		var weatherData WeatherData
+
+		if err := json.Unmarshal(resp.Body(), &weatherData); err != nil {
+			log.Println(err)
+			c.JSON(500, gin.H{
+				"error": "Error processing weather data",
+			})
+			return
+		}
+
+		c.HTML(200, "weather.html", gin.H{
+			"city":        city,
+			"temperature": weatherData.Main.Temp,
+			"description": weatherData.Weather[0].Description,
+		})
+	})
+
+	router.Run(":8080")
+}
