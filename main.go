c277a8d467173573e2e0f205fde7783d242c4e2b
edwinsiby <edwinsibyrajakumary@gmail.com>
2023-10-31 14:42:37 +0530

    structured

--- main.go
+++ main.go
@@ -1,34 +1,12 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"log"
-	"os"
+	"weather/handler"
 
 	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
-	"gopkg.in/resty.v1"
 )
 
-type WeatherData struct {
-	Main struct {
-		Temp float64 `json:"temp"`
-	} `json:"main"`
-	Weather []struct {
-		Description string `json:"description"`
-	} `json:"weather"`
-}
-
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-
-	apiKey := os.Getenv("OPENWEATHERMAP_API_KEY")
-
-	fmt.Println(apiKey)
 
 	router := gin.Default()
 
@@ -38,48 +16,7 @@ func main() {
 		c.HTML(200, "index.html", nil)
 	})
 
-	router.POST("/weather", func(c *gin.Context) {
-		city := c.PostForm("city")
-
-		client := resty.New()
-		resp, err := client.R().
-			SetQueryParams(map[string]string{
-				"q":     city,
-				"appid": apiKey,
-			}).
-			Get("https://api.openweathermap.org/data/2.5/weather")
-
-		if err != nil {
-			log.Println(err)
-			c.JSON(500, gin.H{
-				"error": "An error occurred while fetching weather data",
-			})
-			return
-		}
-
-		if resp.StatusCode() != 200 {
-			c.JSON(resp.StatusCode(), gin.H{
-				"error": "Failed to fetch weather data",
-			})
-			return
-		}
-
-		var weatherData WeatherData
-
-		if err := json.Unmarshal(resp.Body(), &weatherData); err != nil {
-			log.Println(err)
-			c.JSON(500, gin.H{
-				"error": "Error processing weather data",
-			})
-			return
-		}
-
-		c.HTML(200, "weather.html", gin.H{
-			"city":        city,
-			"temperature": weatherData.Main.Temp,
-			"description": weatherData.Weather[0].Description,
-		})
-	})
+	router.POST("/weather", handler.Weather)
 
 	router.Run(":8080")
 }
